43e73e158feb94b84df98d102b3e1549906b0b61
Davide <davide.mattioli.2001@gmail.com>
2024-06-23 00:33:23 +0200

    Update

--- src/Main.rs
+++ src/Main.rs
@@ -4,26 +4,29 @@ fn main() {
     println!("Hello, world!");
             
             struct Actor {
-                money : i32,
+                money : f32,
                 name: String,
             }
 
             impl Actor {
-                fn new(money: i32, name: String) -> Actor {
+                fn new(money: f32, name: String) -> Actor {
                     Actor { money,name }
+                }
+                fn buy(&mut self, amount: f32) {
+                    self.money -= amount;
                 }
 
                 fn nome(&self) {
                     println!("{}", self.name);
                 }
-
-                fn money_val(&self) -> i32 {
+                fn money_val(&self) -> f32 {
                     return self.money;
                     
                 }
             }
 
-            let my_class = Actor::new(rand::thread_rng().gen_range(0..50), "Test".into());
-            my_class.nome();
+            let mut my_class = Actor::new(rand::thread_rng().gen_range(0.0..50.0), "Test".into());
+
+            my_class.buy(-10.0);
             println!("{}", my_class.money_val());
         }
