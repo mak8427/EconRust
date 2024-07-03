f4e804f7bdd98d1b09f3e180ece7c475bec357e5
Davide <davide.mattioli.2001@gmail.com>
2024-07-03 23:12:14 +0200

    Added multi reference

--- src/Main.rs
+++ src/Main.rs
@@ -21,7 +21,7 @@ fn main() {
     let mut actor_1 = Rc::new(RefCell::new(OtherActor::new(rand::thread_rng().gen_range(0.0..50.0), "Test".into(), 20)));
     
     let mut market_1 = Rc::new(RefCell::new(OtherMarket::new()));
-    let mut workplace_1 = Rc::new(RefCell::new(Workplace::new(HashMap::new(), "Test".into())));    // Market initialization
+    let mut workplace_1 = Rc::new(RefCell::new(Workplace::new(HashMap::new(), "Test".into(), 1.0)));    // Market initialization
     market_1.borrow_mut().add_good(10.0, "Potatos".into());
 
     workplace_1.borrow_mut().add_worker(actor_1.clone());
@@ -33,6 +33,7 @@ fn main() {
         actor_1.borrow_mut().buy_needs(market_1.clone());
         actor_1.borrow_mut().increase_population(1);
         workplace_1.borrow_mut().produce();
+        
         
         i += 1;
     }
